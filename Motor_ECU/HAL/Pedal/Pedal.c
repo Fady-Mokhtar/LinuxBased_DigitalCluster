00cf0563e6149810bc3ffff7842aac021849dfab
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-05-03 18:09:08 +0300

    Added Pedal files

---
+++ Motor_ECU/HAL/Pedal/Pedal.c
@@ -0,0 +1,8 @@
+/*
+ * Pedal.c
+ *
+ *  Created on: 3 May 2024
+ *      Author: hp
+ */
+
+
---
+++ Motor_ECU/HAL/Pedal/Pedal.h
@@ -0,0 +1,13 @@
+/*
+ * Pedal.h
+ *
+ *  Created on: 3 May 2024
+ *      Author: hp
+ */
+
+#ifndef PEDAL_PEDAL_H_
+#define PEDAL_PEDAL_H_
+
+
+
+#endif /* PEDAL_PEDAL_H_ */
