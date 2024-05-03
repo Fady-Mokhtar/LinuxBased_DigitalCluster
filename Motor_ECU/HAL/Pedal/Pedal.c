d91eaece1615bc411a4f480366851b6f471aaf38
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-05-03 18:17:18 +0300

    Added pedal files

--- Motor_ECU/HAL/Pedal/Pedal.c
+++ Motor_ECU/HAL/Pedal/Pedal.c
@@ -1,8 +1,29 @@
-/*
- * Pedal.c
+/**
+ * @file Pedal.c
+ * @author Ziad Gamalelden (ziad.gamalelden@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-28
  *
- *  Created on: 3 May 2024
- *      Author: hp
+ * @copyright Copyright (c) 2024
+ *
  */
+#include "stm32f1xx.h"
+ADC_HandleTypeDef ADCHandler =
+{
+	 .Instance = ADC1,
+	 .Init.ScanConvMode = ADC_SCAN_DISABLE,
+	 .Init.ContinuousConvMode = ENABLE,
+	 .Init.DiscontinuousConvMode = DISABLE,
+	 .Init.ExternalTrigConv = ADC_SOFTWARE_START,
+	 .Init.DataAlign = ADC_DATAALIGN_RIGHT,
+	 .Init.NbrOfConversion = 1,
+};
 
+uint32_t Pedal_getRead(void)
+{
+	HAL_ADC_PollForConversion(&ADCHandler, 1);
+	uint32_t ADCRead = HAL_ADC_GetValue(&ADCHandler);
 
+	return ADCRead;
+}
--- Motor_ECU/HAL/Pedal/Pedal_cfg.h
+++ Motor_ECU/HAL/Pedal/Pedal_cfg.h
@@ -1,25 +1,43 @@
 /**
- * @file Pedal.h
+ * @file Pedal_cfg.h
  * @author Ziad Gamalelden (ziad.gamalelden@gmail.com)
- * @brief
+ * @brief 
  * @version 0.1
- * @date 2024-04-28
- *
+ * @date 2024-05-03
+ * 
  * @copyright Copyright (c) 2024
- *
+ * 
  */
 
-#ifndef PEDAL_PEDAL_H_
-#define PEDAL_PEDAL_H_
+#ifndef PEDAL_PEDAL_CFG_H_
+#define PEDAL_PEDAL_CFG_H_
 
 /********************************************************************************************************/
 /************************************************Includes************************************************/
 /********************************************************************************************************/
-#include <stdint.h>
+
 
 /********************************************************************************************************/
 /************************************************Defines*************************************************/
 /********************************************************************************************************/
+
+/**
+ * @brief Lower bound of the pedal press range.
+ *
+ * This macro defines the lower bound of the pedal press range used in the application.
+ * When mapping ADC values to the pedal press range, the minimum ADC value is mapped to this lower bound.
+ */
+#define PEDAL_LOWER_BOUND 0
+
+/**
+ * @brief Upper bound of the pedal press range.
+ *
+ * This macro defines the upper bound of the pedal press range used in the application.
+ * When mapping ADC values to the pedal press range, the maximum ADC value is mapped to this upper bound.
+ */
+#define PEDAL_UPPER_BOUND 100
+
+
 
 
 
@@ -35,7 +53,5 @@
 
 
 
-uint32_t Pedal_getRead(void);
 
-
-#endif /* PEDAL_PEDAL_H_ */
+#endif /* PEDAL_PEDAL_CFG_H_ */
