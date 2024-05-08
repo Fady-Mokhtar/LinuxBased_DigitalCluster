ea035edda343531778024e890e56484cbff1d76d
Ahmed Osman <ahmed.osman0072@gmail.com>
2024-05-08 17:34:41 +0300

    Added Motor Driver Code

---
+++ motorECU/HAL/DC_MOTOR/DC_MOTOR_cfg.h
@@ -0,0 +1,10 @@
+
+
+#ifndef DC_MOTOR_CFG_H_
+#define DC_MOTOR_CFG_H_
+
+#include "../DC_MOTOR/DC_MOTOR.h"
+
+extern const DC_MOTOR_CfgType DC_MOTOR_CfgParam[DC_MOTOR_UNITS];
+
+#endif /* DC_MOTOR_CFG_H_ */
