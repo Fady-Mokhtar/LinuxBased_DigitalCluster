1f7350c632f766871307b9a0a331eef972e3b42d
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-05-21 19:08:07 +0300

    Fixed Pedal code

--- motorECU/HAL/Pedal/Pedal_cfg.h
+++ motorECU/HAL/Pedal/Pedal_cfg.h
@@ -22,6 +22,22 @@
 /********************************************************************************************************/
 
 /**
+ * @brief Minimum ADC value corresponding to the minimum pedal press.
+ *
+ * This macro defines the minimum ADC value corresponding to the minimum amount of pressure required on the pedal.
+ * It represents the lowest possible value that the ADC can output.
+ */
+#define PEDAL_LOWER_BOUND_ADC 0
+
+/**
+ * @brief Maximum ADC value corresponding to the maximum pedal press.
+ *
+ * This macro defines the maximum ADC value corresponding to the maximum amount of pressure allowed on the pedal.
+ * It represents the highest possible value that the ADC can output given its 12-bit resolution (4095).
+ */
+#define PEDAL_UPPER_BOUND_ADC 4026
+
+/**
  * @brief Lower bound of the pedal press range.
  *
  * This macro defines the lower bound of the pedal press range used in the application.
